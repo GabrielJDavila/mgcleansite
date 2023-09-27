f3895efd1d2a226f66622a7292695010228d8313
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-09-27 10:06:14 -0500

    small updates to index.css

--- mgcleanpros-site/src/components/Hero.jsx
+++ mgcleanpros-site/src/components/Hero.jsx
@@ -6,15 +6,15 @@ export default function Hero() {
                 <h1 className="hero-title">The Cleaning Pros of Aurora, Il and surrounding Areas</h1>
                 <ul className="cta-container">
                     <li className="cta-item">
-                        <i class="fa-solid fa-circle-check"></i>
+                        <i className="fa-solid fa-circle-check"></i>
                         <p className="cta-text">free in-home walkthrough</p>
                     </li>
                     <li className="cta-item">
-                        <i class="fa-solid fa-circle-check"></i>
+                        <i className="fa-solid fa-circle-check"></i>
                         <p className="cta-text">no-hassle pricing options</p>
                     </li>
                     <li className="cta-item">
-                        <i class="fa-solid fa-circle-check"></i>
+                        <i className="fa-solid fa-circle-check"></i>
                         <p className="cta-text">superior service and detail</p>
                     </li>
                     
