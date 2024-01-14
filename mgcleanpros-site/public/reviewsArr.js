6d46c7d041b84140ed4d41ff238e2f50fb754e18
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-01-14 17:17:30 -0600

    Updated Reviews.jsx
    Added customer reviews for client

---
+++ mgcleanpros-site/public/reviewsArr.js
@@ -0,0 +1,14 @@
+export const reviewsArr = [
+    {
+        name: "Diane H.",
+        text: "Cleaning is an art form. And Marie is an artist. When she comes, the whole house looks clean, smells clean and is clean. I have always been a pretty good housekeeper and fussy with the quality of cleaning from others, but Marie's cleaning is on a whole other level. She works hard, is efficient and is honest. It's a joy to have her come into our home and make it sparkle."
+    },
+    {
+        name: "Sarah G.",
+        text: "Marie and her team have been cleaning for us for about the past 6 months. I can honestly say it has changed my life for the better. I am now able to focus on my family instead of cleaning on my weekends. Marie is reliable, on time, has great communication, and does an excellent job. She is also great with my 15-year-old dog. I highly recommend Marie and her team!"
+    },
+    {
+        name: "Gloria M.",
+        text: "My husband and I are very happy with M&G Clean Pros. They are fast, efficient and thorough. They are very pleasant and accommodating."
+    }
+]
--- mgcleanpros-site/src/components/Reviews.jsx
+++ mgcleanpros-site/src/components/Reviews.jsx
@@ -1,30 +1,31 @@
 import { useState, useRef } from "react"
+import { reviewsArr } from "../../public/reviewsArr"
 export default function Reviews() {
 
-    const data = [
-        {
-            review: "They are the best! Professional, timely, and great customer service.",
-            name: "Ashley T."
-        },
-        {
-            review: "I will never look for other cleaners again. These people are amazing.",
-            name: "Drew B."
-        },
-        {
-            review: "Marie and her team are customer focused; they get the job done and go above and beyond.",
-            name: "Tasha N."
-        },
-        {
-            review: "Amazing work, they always beat my expectations!",
-            name: "Sunali B."
-        }
-    ]
+    // const data = [
+    //     {
+    //         review: "They are the best! Professional, timely, and great customer service.",
+    //         name: "Ashley T."
+    //     },
+    //     {
+    //         review: "I will never look for other cleaners again. These people are amazing.",
+    //         name: "Drew B."
+    //     },
+    //     {
+    //         review: "Marie and her team are customer focused; they get the job done and go above and beyond.",
+    //         name: "Tasha N."
+    //     },
+    //     {
+    //         review: "Amazing work, they always beat my expectations!",
+    //         name: "Sunali B."
+    //     }
+    // ]
 
     
-    const renderedReviews = data.map((item, index) => {
+    const renderedReviews = reviewsArr.map((item, index) => {
         return (
             <div key={index} className="review-item">
-                <p className="review-text">"{item.review}"</p>
+                <p className="review-text">"{item.text}"</p>
                 <p className="review-name">{item.name}</p>
             </div>
         )
@@ -34,8 +35,7 @@ export default function Reviews() {
         <div id="reviews" className="review-section">
             <h2 className="review-section-title">What Customers Say About Us...</h2>
             <div className="reviews-list">
-                <h1>Coming Soon!</h1>
-                {/* {renderedReviews} */}
+                {renderedReviews}
             </div>
         </div>
     )
